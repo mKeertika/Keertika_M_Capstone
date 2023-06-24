478a7f0a5c9fe8710876d63379042915e4c5e3ad
kCoder <114634268+mKeertika@users.noreply.github.com>
2023-06-23 20:33:57 -0500

    navigation linking done at front end

---
+++ src/main/java/org/perscholas/KeertikamSpringBootBlogAppCapstone/controller/MainController.java
@@ -0,0 +1,40 @@
+package org.perscholas.KeertikamSpringBootBlogAppCapstone.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MainController {
+
+
+    @GetMapping("/")
+    public String home(){
+        return "index";
+    }
+
+    @GetMapping("/about")
+    public String about(){
+        return "about";
+    }
+
+    @GetMapping("/contact")
+    public String contact(){
+        return "contact";
+    }
+
+    @GetMapping("/detail")
+    public String detailedBlog(){
+        return "inner";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/create_post")
+    public String createPost(){
+        return "create-post";
+    }
+
+}
